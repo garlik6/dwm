6c58f925fa31ff56302d3196c47be12c85a2e95a
grigory <grigory@example.com>
2023-03-31 15:51:31 +0000

    brave browser name change

--- config.h
+++ config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *browsercmd[]  = { "brave-browser", NULL };
+static const char *browsercmd[]  = { "brave", NULL };
 static const char *keepasscmd[]  = { "keepassxc", NULL };
 
 static const Key keys[] = {
